2a5d0308811beb2828da8484543cbba33963bd51
gianluca-fernandez-QA <gianluca.fernandez@academytrainee.com>
2020-08-06 14:47:01 +0100

    WP-13 #comment Completed Paint scheme requires database to continue

--- Front-End-Code/Java/index.js
+++ Front-End-Code/Java/index.js
@@ -1,15 +1,19 @@
 window.onload = function doSomething() {
-    
-    var x = document.getElementById("demo");   // Get the element with id="demo"
-    
-    
+    let y = document.getElementById("fill")
+    for (let i = 1; i <= 12; i++) {
+        
+      
+    let x = document.createElement("div");   // Get the element with id="demo"
+    x.className =`col-sm-1`;
+    x.id = "demo";
+    for (let i = 1; i <= 7; i++) {
     let div = document.createElement('div');
     div.id="smlColor";
     div.style.backgroundColor = "red";
     x.appendChild(div);
-    let div1 = document.createElement('div');
-    div1.id="smlColor";
-    div1.style.backgroundColor = "blue";
-    x.appendChild(div1);
     }
-
+    
+    y.appendChild(x);
+    }
+    
+}
